ea9faf00fea044315397b0723720b9607e02c1d0
Sourav Purkait <gaminglordsourav@gmail.com>
2020-10-25 21:33:45 +0530

    mergesort iterative

--- src/app/hooks/algorithms/useMergeSort.js
+++ src/app/hooks/algorithms/useMergeSort.js
@@ -1,4 +1,5 @@
 import { useDispatch, useSelector } from "react-redux"
+import { MAX_BAR_HEIGHT } from "../../constants/barOptions"
 import sleep from "../../helpers/sleep"
 import swap from "../../helpers/swap"
 import { updateActiveBars, updateBars, updateSorting } from "../../store/actions/barActions"
@@ -9,52 +10,60 @@ const useMergeSort = () => {
     const bars = useSelector((state) => state.bar.bars)
     const arrayLength = useSelector((state) => state.toolbar.arrayLength)
     const speed = useSelector((state) => state.toolbar.speed)
-    const merge = async (left, right) => {
-        let resultArray = [], leftIndex = 0, rightIndex = 0;
-      
-        // We will concatenate values into the resultArray in order
-        while (leftIndex < left.length && rightIndex < right.length) {
-            dispatch(updateActiveBars([leftIndex, rightIndex], true))
-            await sleep(speed)
-            if (left[leftIndex].value < right[rightIndex].value) {
-                resultArray.push(left[leftIndex]);
-                leftIndex++; // move left array cursor
-            } else {
-                resultArray.push(right[rightIndex]);
-                rightIndex++; // move right array cursor
-            }
-            dispatch(updateActiveBars([leftIndex, rightIndex], false))
-        }
-      
-        // We need to concat here because there will be one element remaining
-        // from either left OR the right
-        return resultArray
-                .concat(left.slice(leftIndex))
-                .concat(right.slice(rightIndex));
-    }
-    const doSorting = async (unsortedArray=bars) => {
-        // No need to sort the array if the array only has one element or empty
-        if (unsortedArray.length <= 1) {
-          return unsortedArray
-        }
-        // In order to divide the array in half, we need to figure out the middle
-        const middle = Math.floor(unsortedArray.length / 2)
-      
-        // This is where we will be dividing the array into left and right
-        const left = unsortedArray.slice(0, middle)
-        const right = unsortedArray.slice(middle)
-      
-        // Using recursion to combine the left and right
-        const mergedArray = await merge(await doSorting(left), await doSorting(right))
-        return mergedArray
-    }
     const mergeSort = async () => {
-        dispatch(updateSorting(true))
-        let newBars = await doSorting(bars);
-        console.log(newBars);
+        let current_size = 1
+        let newBars = bars
+        while (current_size < (newBars.length - 1)) {
+            let left = 0
+            while (left < newBars.length - 1) {
+                let mid = Math.min((left + current_size - 1), (newBars.length-1))
+                let right = (2 * current_size + left - 1 > newBars.length-1) ? newBars.length - 1 : 2 * current_size + left - 1
+                let n1 = mid - left + 1
+                let n2 = right - mid
+                let L = []
+                for(let x=0; x<n1; x++){
+                    L.push(0)
+                }
+                let R = []
+                for(let x=0; x<n1; x++){
+                    R.push(0)
+                }
+                for (let x=0; x<n1; x++){
+                    L[x] = newBars[left + x]
+                }
+                for (let x=0; x<n2; x++){
+                    R[x] = newBars[mid + x + 1] 
+                }
+                let i = 0,
+                    j = 0,
+                    k = left
+                while (i < n1 && j < n2) {
+                    let indices = [k, newBars.indexOf(L[i]), newBars.indexOf(R[j])]
+                    indices = [...new Set(indices)]
+                    dispatch(updateActiveBars(indices, true))
+                    await sleep(speed*5)
+                    if (L[i].value > R[j].value){
+                        newBars[k++] = R[j++]
+                    } else {
+                        newBars[k++] = L[i++]
+                    }
+                    dispatch(updateActiveBars(indices, false))
+                    await sleep(speed*5)
+                    dispatch(updateBars(newBars))
+                }
+                while (i < n1) {
+                    newBars[k++] = L[i++]
+                }
+                while (j < n2) {
+                    newBars[k++] = R[j++] 
+                }
+                left = left + current_size*2
+            }
+            current_size = 2 * current_size 
+        }
         dispatch(updateBars(newBars))
         dispatch(updateSorting(false))
-        dispatch(finishSorting(bars))
+        dispatch(finishSorting(newBars))
     }
     return { mergeSort }
 }
