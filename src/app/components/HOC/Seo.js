bf05c886a9b5450f9b1757c76a5c350f93a39434
Sourav Purkait <sourav@hubilo.com>
2021-09-29 23:25:26 +0530

    analytics id updated

--- src/app/components/HOC/Seo.js
+++ src/app/components/HOC/Seo.js
@@ -5,7 +5,7 @@ import ReactGA from 'react-ga';
 const Seo = (props) => {
     const { children } = props
     useEffect(()=>{
-        ReactGA.initialize('G-8MS9G8WQEQ');
+        ReactGA.initialize('UA-208882411-1');
         ReactGA.pageview(window.location.pathname + window.location.search);
     },[])
 
